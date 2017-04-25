953e50a1dcc2e4643b541976aab516308d2792e3
shirley tan <shirleytan@google.com>
2017-04-25 16:43:32 -0700

    add background for remote display setting

--- scripts/setup_osx.sh
+++ scripts/setup_osx.sh
@@ -3,6 +3,7 @@
 if [ "${TRAVIS_OS_NAME}" == "osx" ]
   then
     brew cask install google-chrome 
-    sudo Xvfb :99 -ac -screen 0 1024x768x8
+    sudo Xvfb :99 -ac -screen 0 1024x768x8 &
     export CHROME_BIN="/Applications/Google Chrome.app" 
+    echo "debugging"
 fi

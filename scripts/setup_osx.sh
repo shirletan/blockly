52150433e70e3defcb20efa7cefbbf0e7aed5d79
shirley tan <shirleytan@google.com>
2017-04-25 17:03:11 -0700

    remove logging

--- scripts/setup_osx.sh
+++ scripts/setup_osx.sh
@@ -5,5 +5,4 @@ if [ "${TRAVIS_OS_NAME}" == "osx" ]
     brew cask install google-chrome 
     sudo Xvfb :99 -ac -screen 0 1024x768x8 &
     export CHROME_BIN="/Applications/Google Chrome.app" 
-    echo "debugging"
 fi
